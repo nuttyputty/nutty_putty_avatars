7e1bcfd068a06dbc0055810db375b74aad0cb962
Roman Kuzio <rkuzo973@gmail.com>
2020-03-16 15:57:01 +0200

    added log

--- lib/nutty_putty_avatars.dart
+++ lib/nutty_putty_avatars.dart
@@ -347,6 +347,13 @@ class AvatarState extends State<Avatar> {
           }
           return item;
         }).toList();
+
+        print('HAT HAIRS');
+        print(hatHairs);
+
+        print('HAIRS');
+        print(hairs);
+
         print('[ITEM] $item');
         var index = hatHairs.indexOf(person['hair']['element']);
         print('[INDEX1] $index');
